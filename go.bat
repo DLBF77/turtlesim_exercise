d1c3b6beb0d9adfaee350f983cf9406cef43b815
DLBF77 <98495283+DLBF77@users.noreply.github.com>
2022-02-11 18:30:21 -0500

    cleanup

--- go.bat
+++
@@ -1,5 +0,0 @@
-cd turtle_ws
-catkin_make
-call .\devel\setup.bat
-rosrun turtlesim_exercise .\src\turtlesim_exercise\src\turtle_server.py
-
--- turtle_ws/turtle_ws/build/turtlesim_exercise/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ws/turtle_ws/src/turtlesim_exercise
-# Build directory: C:/ws/turtle_ws/build/turtlesim_exercise
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
